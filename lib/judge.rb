08feab4e7fc2006f0075fcc91ef83edff73c52bb
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-04-20 13:45:22 +0200

    Slightly changed judge.rb to disable some RuboCop

--- lib/judge.rb
+++ lib/judge.rb
@@ -5,28 +5,26 @@ class Judge
   def check_code(guess, code) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength
     black_pegs = 0
     white_pegs = 0
-    used_guess_indices = []
     used_code_indices = []
-
-    # check for black pegs
+    used_guess_indices = []
+    # Phase 1: Count black pegs (correct color and position)
     guess.each_with_index do |color, i|
-      next unless color == code[i]
-
-      black_pegs += 1
-      used_code_indices.push(i)
-      used_guess_indices.push(i)
+      if color == code[i] # rubocop:disable Style/Next
+        black_pegs += 1
+        used_code_indices.push(i)
+        used_guess_indices.push(i)
+      end
     end
-
-    # check for white pegs
+    # Phase 2: Count white pegs (correct color, wrong position)
     guess.each_with_index do |color_guess, i| # rubocop:disable Style/CombinableLoops
-      next if used_guess_indices.include?(i)
-
-      code.each_with_index do |color_code, j|
-        next unless !used_code_indices.include?(j) && color_guess == color_code
-
-        white_pegs += 1
-        used_code_indices.push(j)
-        break
+      if !used_guess_indices.include?(i) # rubocop:disable Style/NegatedIf,Style/Next
+        code.each_with_index do |color_code, j|
+          if !used_code_indices.include?(j) && color_guess == color_code # rubocop:disable Style/Next
+            white_pegs += 1
+            used_code_indices.push(j)
+            break
+          end
+        end
       end
     end
     { black: black_pegs, white: white_pegs }
